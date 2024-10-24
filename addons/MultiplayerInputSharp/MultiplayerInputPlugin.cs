88e33739537ea1cb3d6f4b853e69f9424c7f0ee8
Ataman <git2.ata@bhtp.net>
2024-10-24 15:24:16 +0200

    Remove empty overrides in MultiplayerInputPlugin.cs

--- addons/MultiplayerInputSharp/MultiplayerInputPlugin.cs
+++ addons/MultiplayerInputSharp/MultiplayerInputPlugin.cs
@@ -4,14 +4,6 @@ using Godot;
 [Tool]
 public partial class MultiplayerInputPlugin : EditorPlugin
 {
-    public override void _EnterTree()
-	{
-		
-	}
 
-	public override void _ExitTree()
-	{
-		
-	}
 }
 #endif
